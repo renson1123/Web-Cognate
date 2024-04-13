3bf29743a07b633a07498c01ce7e80db0e890e01
Renson S. Placino <placinorenson1123@gmail.com>
2024-04-13 14:35:38 +0800

    Creating Multi Auth for both Admin and User

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::insert([
+            [
+                'name' => 'Admin',
+                'email' => 'admin@gmail.com',
+                'role' => 'admin',
+                'password' => Hash::make('password') // password
+            ],
+            [
+                'name' => 'User',
+                'email' => 'user@gmail.com',
+                'role' => 'user',
+                'password' => Hash::make('password') // password
+            ],
+
+        ]);
+    }
+}
